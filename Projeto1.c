a5c48e3fe7ee60c28b0955deb79d5e8e14c6b1cb
guilhermeKuffner <68231357+guilhermeKuffner@users.noreply.github.com>
2022-09-01 17:29:19 -0300

    Começando em C
    teste de um script simples que apenas printa a imagem de um robô desenhado linha por linha com um "for" para repetir diversas vezes

---
+++ Projeto1.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+ {
+	int d;
+	
+	for(d=0;d<=200;d++)
+	{
+		system.sleep(100);
+		printf("\n\n");
+		sleep(0.01);
+	    printf("\n                    \xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB");
+	    sleep(0.01);
+		printf("\n                    \xBA \xC9\xCD\xBB   \xC9\xCD\xBB \xBA");
+	   	sleep(0.01); 
+		printf("\n                    \xBA \xC8\xCD\xBC   \xC8\xCD\xBC \xBA");
+	   	sleep(0.01); 
+		printf("\n                    \xBA    \xC9\xCD\xBB    \xBA");
+	  	sleep(0.01);
+	    printf("\n                    \xBA    \xC8\xCD\xBC    \xBA");
+		sleep(0.01);    
+		printf("\n                    \xBA   \xC9\xCD\xCD\xCD\xBB   \xBA");
+	  	sleep(0.01);  
+		printf("\n                    \xBA   \xC8\xCD\xCD\xCD\xBC   \xBA");
+	    sleep(1);
+		printf("\n                    \xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC");
+		sleep(1);    
+		printf("\n                         \xBA \xBA");
+	   	sleep(1); 
+		printf("\n                 \xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB");
+	   	sleep(1); 
+		printf("\n                 \xBA                 \xBA");
+	   	sleep(1); 
+		printf("\n                 \xBA                 \xBA");
+	   	sleep(1); 
+		printf("\n        \xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD \xBA                 \xBA \xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB");
+	   	sleep(1); 
+		printf("\n        \xC8\xCD\xBB      \xBA                 \xBA      \xC9\xCD\xBC");
+	   	sleep(1); 
+		printf("\n        \xC9\xCD\xBC      \xBA                 \xBA      \xC8\xCD\xBB");
+	   	sleep(1); 
+		printf("\n        \xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD \xBA                 \xBA \xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC");
+	   	sleep(1); 
+		printf("\n                 \xBA                 \xBA");
+	   	sleep(1); 
+		printf("\n                 \xBA                 \xBA");
+	   	sleep(1); 
+		printf("\n                 \xBA                 \xBA");
+	   	sleep(1); 
+		printf("\n                 \xBA                 \xBA");
+	   	sleep(1); 
+		printf("\n                 \xBA                 \xBA");
+	   	sleep(1); 
+		printf("\n                 \xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC");
+	   	sleep(1); 
+		printf("\n                   \xBA   \xBA     \xBA   \xBA");
+	   	sleep(1); 
+		printf("\n                   \xBA   \xBA     \xBA   \xBA");
+	   	sleep(1); 
+		printf("\n                   \xBA   \xBA     \xBA   \xBA");
+	   	sleep(1); 
+		printf("\n                   \xBA   \xBA     \xBA   \xBA");
+	   	sleep(1); 
+		printf("\n                   \xBA   \xBA     \xBA   \xBA");
+	   	sleep(1); 
+		printf("\n                   \xBA   \xBA     \xBA   \xBA");
+	   	sleep(1); 
+		printf("\n                   \xBA   \xBA     \xBA   \xBA");
+	   	sleep(1); 
+		printf("\n                   \xBA   \xBA     \xBA   \xBA");
+	   	sleep(1); 
+		printf("\n                   \xBA   \xBA     \xBA   \xBA");
+	    //printf("\n               \xC8\xCD\xCD\xCD\xBC     \xC8\xCD\xCD\xCD\xBC");
+	   	sleep(1);
+	    printf("\n       \xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCA\xCD\xCD\xCD\xCA\xCD\xCD\xCD\xCD\xCD\xCA\xCD\xCD\xCD\xCA\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB");
+	   	sleep(1);
+	    printf("\n   \xC9\xCD\xCD\xCD\xCA\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCA\xCD\xCD\xCD\xBB");
+	   	sleep(1); 
+		printf("\n   \xBA                                                \xBA");
+	   	sleep(1); 
+		printf("\n   \xBA               \xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB                   \xBA");
+	   	sleep(1); 
+		printf("\n   \xBA               \xBA   GALVAO   \xBA                   \xBA");
+	   	sleep(1); 
+		printf("\n   \xBA               \xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC                   \xBA");
+	   	sleep(1); 
+		printf("\n   \xBA                                                \xBA");
+	   	sleep(1); 
+		printf("\n   \xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC\n");
+	}
+	system("pause");
+}
